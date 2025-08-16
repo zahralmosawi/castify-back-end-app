17b88bd6758593cd110a80138b1f0810f9793f7e
Zainab <zainnalthamer@gmail.com>
2025-08-16 21:08:40 +0300

    fixed runtime errors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const userRoutes = require('./routes/user.routes');
 const podcastRoutes = require('./routes/podcast.routes');
 const boardRoutes = require('./routes/board.routes');
-const authRoutes = require('routes/auth.routes');
+const authRoutes = require('./routes/auth.routes');
 
 dotenv.config();
 
