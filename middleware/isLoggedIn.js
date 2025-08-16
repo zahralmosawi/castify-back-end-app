e749d0ec0520b150e094fe6dbc30711b7c80159c
Zainab <zainnalthamer@gmail.com>
2025-08-16 19:15:05 +0300

    deleted middleware

--- middleware/isLoggedIn.js
+++
@@ -1,8 +0,0 @@
-function isLoggedIn(req, res, next) {
-    if(!req.session.user) {
-        return res.redirect('/auth/login');
-    }
-    next();
-}
-
-module.exports = isLoggedIn;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const bcrypt = require('bcrypt');
 
 const User = require('../models/User');
-const isLoggedIn = require('../middleware/isLoggedIn');
 
 const multer = require('multer');
 const { avatarStorage } = require('../config/cloudinary');
