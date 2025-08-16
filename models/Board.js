a37049af3b680a64fa819497d54fd965c2d43925
zahralmosawi <zalmosawii65@gmail.com>
2025-08-16 10:41:42 +0300

    added board model

---
+++ models/Board.js
@@ -0,0 +1,21 @@
+const {Schema, model} = require('mongoose')
+const Podcast = require('./Podcast')
+
+const BoardSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    podcasts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Podcast'
+    }],
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+})
+
+const Board = mongoose.model('Board', BoardSchema)
+module.exports = Board
