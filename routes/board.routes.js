b67b4ba9cd191f30f3f700d99592b8b046ebe21e
zahralmosawi <zalmosawii65@gmail.com>
2025-08-17 09:35:40 +0300

    added get single board request

--- routes/board.routes.js
+++ routes/board.routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const boardController = require('../controllers/Boards')
 
 router.get('/', boardController.getAllBoards)
+router.get('/:id', boardController.getSingleBoard)
 router.post('/new', boardController.creatNewBoard)
 
 module.exports = router
