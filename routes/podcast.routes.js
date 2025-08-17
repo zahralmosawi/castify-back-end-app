a46d1bc0919b174bafe3dad05b2917a74f09dd67
Zainab <zainnalthamer@gmail.com>
2025-08-17 21:23:48 +0300

    updated secure route

--- routes/podcast.routes.js
+++ routes/podcast.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = require('express').Router();
 const { getAllPodcasts, showPodcast } = require('../controllers/Podcasts');
+const secureRoute = require('../middleware/secureRoute');
 
-router.get('/podcasts', getAllPodcasts);
+router.get('/podcasts', secureRoute, getAllPodcasts);
 router.get('/podcasts/:podcastId', showPodcast);
 
 module.exports = router;
