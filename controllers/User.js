b0bf478b3e6f891822ccf2668579fd9e9e373873
Zainab <zainnalthamer@gmail.com>
2025-08-18 14:56:21 +0300

    added edit profile route

--- controllers/User.js
+++ controllers/User.js
@@ -15,6 +15,27 @@ async function getProfile(req, res) {
     }
 }
 
+async function updateProfile(req, res) {
+    try {
+        const updates = req.body;
+        
+        const user = await User.findByIdAndUpdate(
+            req.user.id,
+            updates,
+            {new: true}
+        )
+
+        if (!user) {
+            return res.status(404).json({error: "User not found"});
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+}
+
 module.exports = {
-    getProfile
+    getProfile,
+    updateProfile
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = require('express').Router();
 
 const secureRoute = require('../middleware/secureRoute');
-const { getProfile } = require('../controllers/User');
+const { getProfile, updateProfile } = require('../controllers/User');
 
 router.get('/profile', secureRoute, getProfile);
+router.put('/profile', secureRoute, updateProfile);
 
 module.exports = router;
