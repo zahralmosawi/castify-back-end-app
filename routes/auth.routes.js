16761164cc89afeb9a449e10880365c869ccc2c5
zahralmosawi <zalmosawii65@gmail.com>
2025-08-19 07:04:04 +0300

    updated auth routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const authController = require('../controllers/Auth');
 const multer = require('multer');
 const { avatarStorage } = require('../config/cloudinary');
+const secureRoute = require('../middleware/secureRoute');
 const upload = multer({ storage: avatarStorage });
 
 router.post('/signup', upload.single('avatar'), authController.register);
 router.post('/login', authController.login);
+router.post('/changePassword', secureRoute, authController.changePassword)
 
 module.exports = router;
